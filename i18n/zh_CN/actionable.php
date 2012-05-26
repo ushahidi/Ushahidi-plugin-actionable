e53513fa26f7d231afc437e7eb4cb708262dc770
PoPoutdoor <PoPoutdoor@gmail.com>
2012-05-27 01:33:24 +0800

    first commit

---
+++ i18n/zh_CN/actionable.php
@@ -0,0 +1,21 @@
+<?php
+	$lang = array(
+		// main
+		'actionable_head' => '关注事',
+		'actionable' => '关注事项',
+		'urgent'	=> '紧急',
+		'acted'	=> '行动',
+		// admin - report
+		'not_actionable' => '不关注的',
+		// report form
+		'report_actionable'	=> '关注：',
+		'report_actionable_help'	=> '选择一个关注提示项。',
+		'un_actionable'	=> '不关注',
+		'report_acted'	=> '行动：',
+		'report_acted_help'	=> '采取行动后点选，同时在下面输入行动摘要。',
+		// report view
+		'view_actionable'	=> '关注',
+		'view_urgent'	=> '紧急',
+		'view_acted'	=> '行动',
+		'view_summary'	=> '摘要：',
+);
--- views/actionable_form.php
+++ views/actionable_form.php
@@ -2,21 +2,21 @@
 <!-- report is actionable -->
 <div class="row">
 	<h4>
-		Actionable:
-    <span>Check if responders can act on this information.</span>
-  </h4>
-    <?php print form::radio('actionable', '0', $actionable == 0); ?> Un-actionable
-    <?php print form::radio('actionable', '1', $actionable == 1); ?> Actionable
-		<?php print form::radio('actionable', '2', $actionable == 2); ?> Urgent
+		<?php echo Kohana::lang('actionable.report_actionable');?>
+		<span><?php echo Kohana::lang('actionable.report_actionable_help');?></span>
+	</h4>
+    <?php print form::radio('actionable', '0', $actionable == 0); ?>&nbsp;<?php echo Kohana::lang('actionable.un_actionable');?>
+    <?php print form::radio('actionable', '1', $actionable == 1); ?>&nbsp;<?php echo Kohana::lang('actionable.actionable');?>
+	<?php print form::radio('actionable', '2', $actionable == 2); ?>&nbsp;<?php echo Kohana::lang('actionable.urgent');?>
 </div>
 <!-- / report is actionable -->
 
 <!-- report is acted on -->
 <div class="row">
-  <h4>
-    Action Taken: <?php print form::checkbox('action_taken', '1', $action_taken); ?> 
-    <span>Check if action was taken and enter the action summary.</span>
-  </h4>
+	<h4>
+		<?php echo Kohana::lang('actionable.report_acted');?>&nbsp;<?php print form::checkbox('action_taken', '1', $action_taken); ?>
+		<span><?php echo Kohana::lang('actionable.report_acted_help');?></span>
+	</h4>
   <textarea name="action_summary" id="action_summary" style=" height: 60px;"><?php echo $action_summary; ?></textarea>
 </div>
 <!-- / report is acted on -->
